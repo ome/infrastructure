1bd59dd427c375a20ea579053de466c04ae17bfc
Simon Li <spli@dundee.ac.uk>
2016-03-24 10:51:15 +0000

    Make most commands quiet, redirect stderr to stdout

--- ansible/examples/idr-example-omero-bootstrap.sh
+++ ansible/examples/idr-example-omero-bootstrap.sh
@@ -3,13 +3,14 @@
 set -e
 
 # gcc needed for ansible dependencies
-yum install -y git python-virtualenv gcc
-virtualenv --system-site-packages /opt/ansible
-/opt/ansible/bin/pip install ansible
+yum install -q -y git python-virtualenv gcc 2>&1
+virtualenv -q --system-site-packages /opt/ansible 2>&1
+/opt/ansible/bin/pip -q install ansible 2>&1
 
-git clone --single-branch -b omego https://github.com/manics/infrastructure.git
+git clone -q --single-branch -b omego https://github.com/manics/infrastructure.git 2>&1
 ANSIBLE_VARS='{"omero_release": "5.2.2" }'
 #ANSIBLE_VARS='{"omero_release": "OMERO-build", "omero_omego_additional_args": "-qq --ci alternative-ci.openmicroscopy.org:8080"}'
 
-ANSIBLE_ROLES_PATH=./infrastructure/ansible/roles /opt/ansible/bin/ansible-playbook \
-    ./infrastructure/ansible/examples/idr-example-omero.yml --extra-vars "$ANSIBLE_VARS"
+export ANSIBLE_ROLES_PATH=./infrastructure/ansible/roles
+/opt/ansible/bin/ansible-playbook ./infrastructure/ansible/examples/idr-example-omero.yml \
+    --extra-vars "$ANSIBLE_VARS" 2>&1
