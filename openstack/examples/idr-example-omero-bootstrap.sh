ac7a75b72bd54c2cdf6601e4b85bb42deebefdd2
Simon Li <spli@dundee.ac.uk>
2016-04-19 11:24:44 +0100

    Go back to vars file, autodetect selinux
    Use a file for extra vars where possible, as this avoid complicated quoting rules for variables with spaces.

--- openstack/examples/idr-example-omero-bootstrap.sh
+++ openstack/examples/idr-example-omero-bootstrap.sh
@@ -13,14 +13,20 @@ cd /opt
 [ -d /opt/infrastructure ] || git clone -q --single-branch -b omego https://github.com/manics/infrastructure.git /opt/infrastructure 2>&1
 
 # Custom variable overrides (YAML, can be empty)
-cat << EOF > /opt/infrastructure/localhost-extravars.yml
-omero_release: "5.2.2"
-#omero_release: OMERO-build
+# Use this instead of command line args to avoid complicated quoting issues
+VARSFILE=/opt/infrastructure/localhost-extravars.yml
+cat << EOF > $VARSFILE
+#omero_release: "5.2.2"
+omero_release: OMERO-DEV-merge-build
 #omero_omego_additional_args: "--ci alternative-ci.openmicroscopy.org:8080"
 EOF
 
+if [ $(getenforce) = Disabled ]; then
+    echo "omero_selinux_setup: False" >> $VARSFILE
+fi
+
 export ANSIBLE_ROLES_PATH=/opt/infrastructure/ansible/roles
 ansible-playbook /opt/infrastructure/openstack/examples/idr-example-omero.yml \
-    --extra-vars "omero_release=OMERO-DEV-merge-build omero_selinux_setup=False @/opt/infrastructure/localhost-extravars.yml" 2>&1
+    --extra-vars "@/opt/infrastructure/localhost-extravars.yml" 2>&1
 
 exit 0
