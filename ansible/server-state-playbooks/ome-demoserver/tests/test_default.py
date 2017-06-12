f6d811a89217b4023886e35ba48fd11b2121c7af
Kenneth Gillen <k.h.gillen@dundee.ac.uk>
2017-06-12 17:28:08 +0100

    Removing non-working test, as webclient not public in this mixed public/non-public server.

--- ansible/server-state-playbooks/ome-demoserver/tests/test_default.py
+++ ansible/server-state-playbooks/ome-demoserver/tests/test_default.py
@@ -5,13 +5,3 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 OMERO = '/opt/omero/web/OMERO.web/bin/omero'
-
-
-def test_omero_web_public(Command):
-    out = Command.check_output(
-        'curl http://localhost/webclient/api/containers/')
-    r = json.loads(out)
-    assert r['screens'] == []
-    assert r['plates'] == []
-    assert r['projects'] == []
-    assert r['datasets'] == []
