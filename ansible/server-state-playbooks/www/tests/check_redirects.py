59cd30a5a6ac5d2411a40ef72d2284b79c21fe12
Sebastien Besson <seb.besson@gmail.com>
2017-08-30 10:58:50 +0100

    Add redirects for OMERO and Bio-Formats downloads pages
    Currently these pages are redirected to the new website via www-legacy. This
    commit adds direct redirects alongside with the corresponding tests.

--- ansible/server-state-playbooks/www/tests/check_redirects.py
+++ ansible/server-state-playbooks/www/tests/check_redirects.py
@@ -29,7 +29,9 @@ redirect_uris = [
 
     ('/site/products', '/products'),
     ('/site/products/bio-formats', '/bio-formats'),
+    ('/site/products/bio-formats/downloads', '/bio-formats/downloads'),
     ('/site/products/omero', '/omero'),
+    ('/site/products/omero/downloads', '/omero/downloads'),
     ('/site/products/omero/feature-list', '/omero/new'),
 
     ('/site/support', '/docs'),
